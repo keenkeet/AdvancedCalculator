20c2b8ea3a28ef328d0a4c60d1c589381d8326a8
keenkeet <keenkeetjoker619@hotmail.com>
2017-07-30 14:56:01 +0800

    Add files via upload

---
+++ src/lab10simplecalculator/Lab10SimpleCalculator.java
@@ -0,0 +1,10 @@
+package lab10simplecalculator;
+
+public class Lab10SimpleCalculator {
+
+    public static void main(String[] args) {
+        CalculatorForm cf = new CalculatorForm();
+        cf.setVisible(true);
+    }
+    
+}
